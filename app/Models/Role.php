ca819ab21960ed83721b314b876b2f1238ac5240
januarpancaran <januar352@gmail.com>
2025-09-24 19:28:05 +0700

    feat(model): adding permission table relationship to role table

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -10,8 +10,30 @@ class Role extends Model
 
     protected $casts = [ 'is_active' => 'boolean' ];
 
+    // Relationship with user
     public function users()
     {
         return $this->hasMany(User::class);
+    }
+
+    // Relationship with permission
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'role_permission');
+    }
+
+    public function hasPermission(string $permission): bool
+    {
+        return $this->permissions()->where('name', $permission)->exists();
+    }
+
+    public function givePermission(Permission $permission)
+    {
+        return $this->permissions()->attach($permission);
+    }
+
+    public function removePermission(Permission $permission)
+    {
+        return $this->permissions()->detach($permission);
     }
 }
