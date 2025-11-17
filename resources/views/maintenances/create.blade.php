1dc9f0e57ccaba727bafa34ef87824cf82807c45
januarpancaran <151476664+januarpancaran@users.noreply.github.com>
2025-11-17 04:36:32 +0000

    format: format files

--- resources/views/maintenances/create.blade.php
+++ resources/views/maintenances/create.blade.php
@@ -22,7 +22,8 @@
                                 class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
                                 @foreach ($properties as $property)
                                     <option value="{{ $property->id }}">{{ $property->title }}
-                                        ({{ $property->address }})</option>
+                                        ({{ $property->address }})
+                                    </option>
                                 @endforeach
                             </select>
                             <x-input-error :messages="$errors->get('property_id')" class="mt-2" />
