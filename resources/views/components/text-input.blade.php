7b48504d7b66998bbcfecf5145a5eb4b99c248ca
AdiPutro13P <rizkyaulia490@gmail.com>
2025-11-19 15:58:31 +0700

    ngubah register dan login

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,4 +1,4 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled)
-    {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input {{ $disabled ? 'disabled' : '' }} 
+    {!! $attributes->merge(['class' => 'border-gray-700 dark:border-gray-700 bg-gray-700 dark:bg-gray-700 text-white dark:text-gray-200 focus:border-orange-500 dark:focus:border-orange-500 focus:ring-orange-500 dark:focus:ring-orange-500 rounded-md shadow-sm']) !!}>
